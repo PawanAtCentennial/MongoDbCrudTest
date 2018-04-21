3a717473958f05cad42652ef6e032d67b90e06f6
Pawan Vig <pvig@my.centennialcollege.ca>
2018-04-21 12:15:57 -0400

    mongoose config m lab

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 // Define the Mongoose configuration method
 module.exports = function() {
 	// Use Mongoose to connect to MongoDB
-	const db = mongoose.connect(config.db);
-
+let URI =	"mongodb://pawan:pawan@ds113749.mlab.com:13749/comp308project";
+	//const db = mongoose.connect(config.db);
+	const db = mongoose.connect(URI);
 	// Load the 'User' model 
     require('../app/models/user.server.model');
     
